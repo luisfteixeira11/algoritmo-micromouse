a7e4e281874bfa77ded010d9ce8d94f9b66939b1
luisfteixeira11 <luisfteixeira117@gmail.com>
2025-11-14 23:01:26 -0200

    add: funcoes para implementar no codigo

--- Main.py
+++ Main.py
@@ -1,13 +1,22 @@
 import API
 import sys
-
+import numpy as np
 
 def log(string):
     sys.stderr.write("{}\n".format(string))
     sys.stderr.flush()
 
+def atualizar_inundação():
+    # implementar a lógica de inundação na matriz
+    pass
+
+def atualizar_parede():
+    # implementar a atualização da parede na célula que o robô está
+    pass
+
 def main():
     log("Running...")
+    paredes = np.zeros(16, 16)
     API.setColor(0, 0, "G")
     API.setText(0, 0, "abc")
     while True:
