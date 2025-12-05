14555051c001e6cbc876cabac3b83e58e62a3c85
INSPIRADO\Giulia <giuliasousamendes@gmail.com>
2025-12-05 13:26:33 -0300

    correções nas funções e situações exclusivas

--- algoritmo/rota_mapeamento.py
+++ algoritmo/rota_mapeamento.py
@@ -10,40 +10,52 @@ def rota_mapeamento(x,y,matriz):
             matriz_concluida = True
         
         #encurralado
-        if API.WallFront() and API.WallLeft() and API.WallRight():
+        if API.wallFront() and API.wallLeft() and API.wallRight():
             API.turnRight90()
             API.turnRight90()
 
         #bifurcação parede na frente
-        elif API.WallFront() and API.WallLeft()==False and API.WallRight()==False:
+        elif API.wallFront() and API.wallLeft()==False and API.wallRight()==False:
             if matriz[x-1][y] == -1:
                 API.turnLeft90()
             else:
                 API.turnRight90()
 
         #bifurcação parede esquerda
-        elif API.WallFront()==False  and API.WallLeft() and API.WallRight()==False:
+        elif API.wallFront()==False and API.wallLeft() and API.wallRight()==False:
             if matriz[x][y-1] == -1:
-                continue
+                pass
             else:
                 API.turnRight90()
 
         #bifurcação parede direita
-        elif API.WallFront()==False  and API.WallLeft()==False and API.WallRight():
+        elif API.wallFront()==False  and API.wallLeft()==False and API.wallRight():
             if matriz[x][y-1] == -1:
-                continue
+                pass
             else:
                 API.turnLeft90()
 
         #trifurcação
-        elif API.WallFront()==False and API.WallLeft()==False and API.WallRight()==False:
+        elif API.wallFront()==False and API.wallLeft()==False and API.wallRight()==False:
             if matriz [x][y-1] == -1:
-                continue
+                pass
             else:
                 if matriz[x-1][y] == -1:
                     API.turnLeft90()
                 else:
                     API.turnRight90()
-                
+
+        #só tem como virar para a direita
+        elif API.wallFront() and API.wallLeft():
+            API.turnRight90()
+
+        #só tem como virar para a esquerda
+        elif API.wallFront() and API.wallRight():
+            API.turnLeft90()
+
+        #só tem seguir em frente
+        elif API.wallRight() and API.wallLeft():
+            pass
+
         #adicionar verificação de matriz concluida > matriz concluida == True
-        return API.moveForward()
+        API.moveForward()
