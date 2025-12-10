34aa59534e036451dd7cc0482cc67558a6b313de
DELL-DE-JLFVS\jmrsn <jamerson.felix@upe.br>
2025-12-09 21:51:43 -0300

    merge

--- algoritmo/Main.py
+++ algoritmo/Main.py
@@ -77,67 +77,6 @@ def main():
         if (y, x) in [(7, 7), (8, 7), (7, 8), (8, 8)]:
             API.log("Voce chegou ao seu destino!!")
             break  
-        
-<<<<<<< HEAD
-        #mapemento
-        while matriz_concluida == False:
-            matriz_parede = atualizar_paredes(matriz_parede, x, y, orientacao)
-
-            #criação e atualização da matriz de inundação
-            matriz_inundacao = np.zeros((16, 16), dtype=int)-1
-            matriz_inundacao = atualizar_inundacao(matriz_inundacao, matriz_parede)
-            
-            #conclusão do mapeamento
-            if not -1 in matriz_parede:
-                matriz_concluida = True
-<<<<<<< HEAD
-                API.log("mapeamento concluído")
-
-                break
-=======
-                API.log("Mapeamento concluido!")
-                API.log("Retornando para o Start...")
-                break
-            
-            #roda escolhida durante o mapeamento
->>>>>>> e3f5d98d9bccac0a84f47c97ce8fb17d4e9fe3fa
-            x, y, orientacao = rota_mapeamento(x, y, matriz_parede, orientacao)
-            
-            API.setColor(x, 15-y, "B")
-<<<<<<< HEAD
-            API.log(matriz_parede)
-            matriz_inundacao = flood_volta(matriz_inundacao, matriz_parede)
-        if (matriz_concluida==True):
-            API.log("venceu")
-        melhor_caminho_para_centro(x, y, orientacao, matriz_inundacao, matriz_parede)
-
-=======
->>>>>>> e3f5d98d9bccac0a84f47c97ce8fb17d4e9fe3fa
-        
-        #atualiza a matriz de flood para a volta ao start
-        matriz_volta = atualizar_flood_volta(matriz_volta, matriz_parede)
-
-        #volta ao start
-        while True:
-            x, y, orientacao = melhor_caminho(x, y, orientacao, matriz_volta, matriz_parede)
-            API.setColor(x, 15-y, "Y")
-            #para quando chega ao centro 
-            if (y, x) in [(15, 0)]:
-                API.log("Micromouse no START!")
-                break
-        
-        #ida ao centro
-        while True:
-            x, y, orientacao = melhor_caminho(x, y, orientacao, matriz_inundacao, matriz_parede)
-            API.setColor(x, 15-y, "R")
-            #para quando chega ao centro 
-            if (y, x) in [(7, 7), (8, 7), (7, 8), (8, 8)]:
-                API.log("Voce chegou ao seu destino!!")
-                break  
-        break
-=======
->>>>>>> origin/Giulia
-
 
 if __name__ == "__main__":
     main()
